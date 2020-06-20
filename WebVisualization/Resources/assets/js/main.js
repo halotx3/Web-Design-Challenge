4fb5a08c01d2e6b86b09696191080cc13c4b6a7c
Christopher Brownlee <cdbreach@gmail.com>
2020-06-20 14:48:50 -0400

    Further solidied base, polish in progress

---
+++ WebVisualization/Resources/assets/js/main.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//     console.log('Page Ready')
+// });
+
+document.getElementById("datab").onclick = function () {
+    location.href = "./data.html";
+};
+document.getElementById("comp1").onclick = function () {
+    location.href = "./comparison.html";
+};
